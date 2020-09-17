da8ffb4b89fd972fdee97489a66b048de0fa4d99
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-09-17 09:33:57 -0300

    AULA 19.8 - Excluindo tarefas

--- src/components/TaskListTable.js
+++ src/components/TaskListTable.js
@@ -8,6 +8,8 @@ class TaskListTable extends Component {
         this.state = {
             tasks: []
         }
+
+        this.onDeleteHandler = this.onDeleteHandler.bind(this);
     }
     
     
@@ -16,7 +18,10 @@ class TaskListTable extends Component {
             <div>
                 <table className="table table-striped">
                     <TableHeader />
-                    <TableBody tasks={this.state.tasks}/>
+                    <TableBody 
+                        tasks={this.state.tasks}
+                        onDelete={this.onDeleteHandler}
+                    />
                 </table>
             </div>
         );
@@ -28,6 +33,11 @@ class TaskListTable extends Component {
 
     listTasks() {
         this.setState({ tasks: TaskService.list() });
+    }
+
+    onDeleteHandler(id) {
+        TaskService.delete(id);
+        this.listTasks();
     }
 }
 
@@ -54,7 +64,7 @@ const TableBody = (props) => {
                     <td>{ task.whenToDo }</td>
                     <td>
                         <input type="button" className="btn btn-primary" value="Editar" />
-                        &nbsp;<input type="button" className="btn btn-danger" value="Excluir" />
+                        &nbsp;<input type="button" className="btn btn-danger" value="Excluir" onClick={() => props.onDelete(task.id)}/>
                     </td>
                 </tr>
             )}
